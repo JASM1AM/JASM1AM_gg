a3e519d4b8c2962a3c7c12f77568f13c76908ec9
JASMIAM <69405366+1498934815@users.noreply.github.com>
2025-09-06 15:39:56 +0800

    更新 agg.lua

--- agg.lua
+++ agg.lua
@@ -1,13 +1,17 @@
-if gg.getRangesList("libanogs.so:bss")[1] then
-	local t = {}
-	t[1] = gg.getRangesList("libanogs.so:bss")[1]["start"] + 0x4F0; -- 数值地址:0x72C5BDA2A0
-	gg.addListItems({
-		[1] = { 
-			address = t[1],
-			flags = 4,
-			value = 4096,
-			freeze = true,
-		},
-	})
-	gg.alert("防闪开启成功")
+while true do
+    if gg.getRangesList("libanogs.so:bss")[1] then
+        local t = {}
+        t[1] = gg.getRangesList("libanogs.so:bss")[1]["start"] + 0x4F0; -- 数值地址:0x72C5BDA2A0
+        gg.addListItems({
+            [1] = { 
+                address = t[1],
+                flags = 4,
+                value = 4096,
+                freeze = true,
+            },
+        })
+        gg.toast("防闪开启成功")
+    else
+        gg.toast("未找到 libanogs.so:bss，等待重试...")
+    end
 end
